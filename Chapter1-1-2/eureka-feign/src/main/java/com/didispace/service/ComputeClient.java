205ef33215b97da63477514643d2f3cc8019a0d5
wwy <wwyknight@163.com>
2017-04-28 10:08:38 +0800

    fix

--- Chapter1-1-2/eureka-feign/src/main/java/com/didispace/service/ComputeClient.java
+++ Chapter1-1-2/eureka-feign/src/main/java/com/didispace/service/ComputeClient.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("compute-service")
 public interface ComputeClient {
-
     @RequestMapping(method = RequestMethod.GET, value = "/add")
     Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
-
 }
